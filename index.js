328b98adbc14b976682874221eeefc3425d689cb
Vinicius Vellozo <vinicius.vellozo@hotmail.com>
2022-02-10 17:37:09 -0300

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`);
+});
